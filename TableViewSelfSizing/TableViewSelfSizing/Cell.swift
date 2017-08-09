024b0820f335ece50e15138128487273244f65f1
iOS Dev Cafe <iosdevcafe@gmail.com>
2017-08-09 20:41:47 +0200

    Added the self sizing table view cell example

---
+++ TableViewSelfSizing/TableViewSelfSizing/Cell.swift
@@ -0,0 +1,20 @@
+//
+//  Cell.swift
+//  TableViewSelfSizing
+//
+//  Created by iOS Café on 09/08/2017.
+//  Copyright © 2017 iOS Café. All rights reserved.
+//
+
+import UIKit
+
+class Cell: UITableViewCell {
+
+    @IBOutlet weak private var myLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
