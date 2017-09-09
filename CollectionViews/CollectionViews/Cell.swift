476844bc310b0e710b0e33dbe8b4c3b5dd3dd445
iOS Dev Cafe <iosdevcafe@gmail.com>
2017-09-09 09:14:52 +0200

    Latest collection view examples

---
+++ CollectionViews/CollectionViews/Cell.swift
@@ -0,0 +1,15 @@
+//
+//  Cell.swift
+//  CollectionViews
+//
+//  Created by iOS Café on 27/08/2017.
+//  Copyright © 2017 iOS Café. All rights reserved.
+//
+
+import UIKit
+
+class Cell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+    
+}
---
+++ CollectionViews/CollectionViews/ImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCell.swift
+//  CollectionViews
+//
+//  Created by iOS Café on 27/08/2017.
+//  Copyright © 2017 iOS Café. All rights reserved.
+//
+
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
